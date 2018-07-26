8bb8b35bb49c24d0e90495cc53d8cad452b99ee0
Eugen Rožić <erozic@zoho.eu>
2018-07-26 12:37:22 +0100

    Cluster_analysis updated for cluster tracking (in lib)
    1) now reads cluster info from dump instead of calculating it
    
    2) output by cluster type (homogeneous or mixed)
    
    TODO: update cluster_statistics.py to conform to these changes

--- tools/cluster_analysis.py
+++ tools/cluster_analysis.py
@@ -10,18 +10,22 @@ import os
 import argparse
 
 import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import connected_components
 
 import lammps_multistate_rods
 
-def state_struct_to_name(state_struct):
+def state_struct_to_id(state_struct):
+    '''
+    Returns rod state id from its structure (list of atom types)
+    '''
     for i in range(model.num_states):
         if state_struct == model.state_structures[i].replace('|',''):
-            return model.rod_states[i]
+            return i
     return None
 
 def wrap_periodic((elem, L)):
+    '''
+    Wraps position (distance) along a periodic boundary.
+    '''
     if elem > L/2.:
         elem -= L
     elif elem < -L/2.:
@@ -29,115 +33,127 @@ def wrap_periodic((elem, L)):
     return elem
 
 def parse_dump_file(dump_file_path):
+    '''
+    Reads the dump file given by the path and extracts data from it.
     
+    returns : a list of [timestep, snapshot_data] pairs, where "snapshot_data" is a dictionary by
+    cluster ID's whose values are lists of (molecule ID, rod state ID) pairs
+    '''
     with open(dump_file_path, 'r') as dump_file:
-        lines = dump_file.readlines()
-    
-    box_size = None
-    data = []
-    i = 0
-    while i < len(lines):
-        timestep = int(lines[i+1])
-        atoms_to_read = int(lines[i+3])
-        pattern = re.compile(r'([-\+\d\.eE]+) ([-\+\d\.eE]+)')
-        xmin, xmax = [float(match) for match in pattern.match(lines[i+5]).groups()]
-        ymin, ymax = [float(match) for match in pattern.match(lines[i+6]).groups()]
-        zmin, zmax = [float(match) for match in pattern.match(lines[i+7]).groups()]
-        if box_size is None:
-            box_size = [(xmax - xmin), (ymax - ymin), (zmax - zmin)]
+        
+        data = []
+        #box_size = []
+        #TODO get info on which column is what, don't force exact format...
+        pattern = re.compile(r'(\d+) ([-\+\d\.eE]+) ([-\+\d\.eE]+) ([-\+\d\.eE]+) (\d+) (\d+) (\d+)')
+        snapshot_data = {}
+        current_mol_id = None
+        current_cluster_id = None
+        current_rod_structure = []
+        i = 0
+        max_row = 0
+        for line in dump_file:
+            i += 1
+            if i == 2:
+                timestep = int(line)
             
-        #TODO get info on which column is what, don't force exact format... just require mol, type, x, y & z ...
-        i += 9
-        active_particles = []
-        mol_states = []
-        pattern = re.compile(r'(\d+) (\d+) ([-\+\d\.eE]+) ([-\+\d\.eE]+) ([-\+\d\.eE]+)')
-        mol_id, bead_type, x, y, z = pattern.match(lines[i]).groups()
-        current_mol_id = int(mol_id)
-        current_mol_data = []
-        current_mol_structure = []
-        for n in range(atoms_to_read):
-            mol_id, bead_type, x, y, z = pattern.match(lines[i+n]).groups()
-            mol_id = int(mol_id)
-            bead_type = int(bead_type)
-            if mol_id != current_mol_id:
-                active_particles.append(current_mol_data)
-                mol_states.append(''.join(str(bead_type) for bead_type in current_mol_structure))
-                current_mol_id = mol_id
-                current_mol_data = []
-                current_mol_structure = []
-            current_mol_structure.append(bead_type)
-            if bead_type in active_types:
-                current_mol_data.append(np.array([float(x), float(y), float(z)]))
-        active_particles.append(current_mol_data)
-        mol_states.append(''.join(str(bead_type) for bead_type in current_mol_structure))
-        
-        data.append( [timestep, (active_particles, map(state_struct_to_name, mol_states))] )
-        
-        i += atoms_to_read
-    
-    return box_size, data
+            elif i == 4:
+                atoms_to_read = int(line)
+                max_row = 9 + atoms_to_read
+            
+#             elif i in (6,7,8) and len(box_size) < 3:
+#                 l_bound, r_bound = [float(match) for match in re.compile(r'([-\+\d\.eE]+) ([-\+\d\.eE]+)').match(line).groups()]
+#                 box_size.append(r_bound - l_bound)
+            
+            elif i >= 10 and i <= max_row:
+                bead_id, x, y, z, bead_type, mol_id, cluster_id = pattern.match(line).groups()
+                mol_id = int(mol_id)
+                cluster_id = int(cluster_id)
+                if current_mol_id is None:
+                    current_mol_id = mol_id
+                elif mol_id != current_mol_id:
+                    current_rod_state = state_struct_to_id(''.join(bead_type for bead_type in current_rod_structure))
+                    try:
+                        snapshot_data[current_cluster_id].append((current_mol_id, current_rod_state))
+                    except KeyError:
+                        snapshot_data[current_cluster_id] = [(current_mol_id, current_rod_state)]
+                    current_mol_id = mol_id
+                    current_rod_structure = []
+                current_rod_structure.append(bead_type)
+                if cluster_id > 0:
+                    current_cluster_id = cluster_id
+            
+            if i == max_row:
+                current_rod_state = state_struct_to_id(''.join(bead_type for bead_type in current_rod_structure))
+                try:
+                    snapshot_data[current_cluster_id].append((current_mol_id, current_rod_state))
+                except KeyError:
+                    snapshot_data[current_cluster_id] = [(current_mol_id, current_rod_state)]
+                data.append([timestep, snapshot_data])
+                snapshot_data = {}
+                current_mol_id = None
+                current_cluster_id = None
+                current_rod_structure = []
+                i = 0
+    return data
 
-def analyse_snapshot((active_particles, mol_states), box_size):
+#TODO provide more advanced analysis (like changes in clusters between snapshots)
+def analyse_data(raw_data):
+    '''
+    Analyses the output of "parse_dump_file"...
     
-    num_rods = len(active_particles)
-    adj_mat = np.zeros((num_rods, num_rods), dtype='int')
-    for i in range(num_rods):
-        if len(active_particles[i]) == 1:
-            delta_i = r_body
-        else:
-            delta_i = r_int
-        adj_mat[i][i] = 1
-        for bead_i in active_particles[i]:
-            for j in range(i, num_rods):
-                if adj_mat[i][j] != 0:
-                    continue
-                if len(active_particles[j]) == 1:
-                    delta_j = r_body 
-                else:
-                    delta_j = r_int
-                delta = delta_i + delta_j + cutoff_dist
-                for bead_j in active_particles[j]:
-                    dist = np.linalg.norm(map(wrap_periodic, zip((bead_i-bead_j),box_size)))
-                    if dist < delta:
-                        adj_mat[i][j] = adj_mat[j][i] = 1
-                        break
-                    
-    adj_mat = csr_matrix(adj_mat)
-    num_clusters, clusters = connected_components(adj_mat, directed=False)
-    cluster_labels, cluster_sizes = np.unique(clusters, return_counts=True)
-    sizes, size_occurences = np.unique(cluster_sizes, return_counts=True)
+    return : a list of [timestep, cluster_sizes] pairs, where "cluster_sizes" is a dictionary by
+    cluster type (same as rod state ID if homogeneous, otherwise -1) whose values are
+    lists of (cluster size, occurrences) pairs 
+    '''
+    cluster_data = []
     
-    #TODO use mol_states for analysis of cluster types, e.g. fibril, micelle or mixed...
+    for timestep, snapshot_data in raw_data:
+        cluster_sizes = {}
+        for cluster in snapshot_data.values():
+            cluster_type = cluster[0][1] # cluster_type is the same as state id of rods if they are all in the same state
+            for rod_id, rod_state in cluster:
+                if rod_state != cluster_type:
+                    cluster_type = -1
+            try:
+                cluster_sizes[cluster_type].append(len(cluster))
+            except KeyError:
+                cluster_sizes[cluster_type] = [len(cluster)]
     
-    ret = [0]*max(sizes)
-    for size,n in zip(sizes,size_occurences):
-        ret[size-1] = n
-    return ret
+        for cluster_type, value in cluster_sizes.iteritems():
+            cluster_sizes[cluster_type] = zip(*np.unique(value, return_counts=True))
+        
+        cluster_data.append([timestep, cluster_sizes])
+        
+    return cluster_data
 
 def analyse_dump_file(dump_file, config_file):
-    #get information about the model/simulation from the config file
-    global model, r_body, r_int, cutoff_dist, active_types
-    model = lammps_multistate_rods.Model(config_file) 
-    r_body = model.rod_radius
-    r_int = model.int_radius
-    cutoff_dist = 0.5*model.rod_radius #??
-    active_types = model.active_bead_types
+    '''
+    return : the output of the "analyse_data" method
+    '''
+    global model
+    model = lammps_multistate_rods.Model(config_file)
 
-    box_size, raw_data = parse_dump_file(dump_file)
-    n_snapshots = len(raw_data)
-
-    cluster_data = raw_data
-
-    for i in range(n_snapshots):
-        cluster_data[i][1] = analyse_snapshot(raw_data[i][1], box_size)
+    raw_data = parse_dump_file(dump_file)
     
-    return cluster_data #a list of [ timestep, [ cluster occurences ] ] {cluster size implicit as index+1}
+    cluster_data = analyse_data(raw_data)
+    
+    return cluster_data
 
 def output_cluster_data(cluster_data, output_path):
     
     with open(output_path, 'w') as out_file:
-        for timestamp, data in cluster_data:
-            out_file.write('{:^10d} | {:s}\n'.format(timestamp, ','.join(str(elem) for elem in data)))
+        for timestamp, cluster_sizes in cluster_data:
+            out_str = []
+            for key, value in cluster_sizes.iteritems():
+                out_str.append('({})'.format(key))
+                i = 1
+                for size, occurrence in value:
+                    while i < size:
+                        out_str.append('0')
+                        i += 1
+                    out_str.append('{:d}'.format(occurrence))
+                    i += 1
+            out_file.write('{:^10d} | {:s}\n'.format(timestamp, ' '.join(out_str)))
 
 def read_cluster_data(in_path):
     
@@ -145,9 +161,22 @@ def read_cluster_data(in_path):
     
     with open(in_path, 'r') as data_file:
         for line in data_file:
-            timestep, data = line.split(' | ')
-            cluster_data.append([ int(timestep), [int(elem) for elem in data.split(',')] ])
-    
+            cluster_sizes = {}
+            timestep, raw_data = line.split(' | ')
+            chunks = re.compile(r'\(([-\d]+)\) ([ \d]+)').findall(raw_data)
+            for chunk in chunks:
+                key = int(chunk[0])
+                value = []
+                i = 1
+                for elem in chunk[1].split(' '):
+                    if elem != '':
+                        elem = int(elem)
+                        if elem > 0:
+                            value.append((i,elem))
+                        i += 1
+                cluster_sizes[key] = value
+            cluster_data.append([int(timestep), cluster_sizes])
+            
     return cluster_data
 
 # if called as a program does the cluster analysis of a single dump file
@@ -155,13 +184,15 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='''
         Application for the analysis of clusters of lammps_multistate_rods as molecules
-        from LAMMPS dump files with the structure: mol type x y z
-        ''')
-    parser.add_argument('config_file', help='path to the "lammps_multistate_rods" model config file describing the molecules')
+        from LAMMPS dump files with the output structure: id x y z type mol c_''' +
+        lammps_multistate_rods.Simulation.cluster_compute)
+    parser.add_argument('config_file', help='path to the "lammps_multistate_rods" model config file')
     parser.add_argument('in_file', help='path of the dump file to analyse')
     args = parser.parse_args()
     
+    output_path = os.path.splitext(args.in_file)[0]+"_cluster_data"
+    
     processed_data = analyse_dump_file(args.in_file, args.config_file)
 
-    output_cluster_data(processed_data, os.path.splitext(args.in_file)[0]+"_cluster_data")
+    output_cluster_data(processed_data, output_path)
         
