91b3398bbc086dd3e55d289a29f6d2b2881b5aed
Eugen Rožić <erozic@zoho.eu>
2018-07-28 01:22:00 +0100

    Cluster tools updated
    1) cluster_analysis now supports cluster tracking and outputs data in a
    different, more rich format (all rods by cluster, with states)
    
    1.1) also it offers methods for use on the data (analysis; more to be
    added later)
    
    1.2) can now process multiple dump files as a standalone program
    
    
    2) cluster_statistics now uses the new format of cluster_analysis and
    now generates graphs (static and interactive for distributions) by
    cluster type (for all types)

--- tools/cluster_analysis.py
+++ tools/cluster_analysis.py
@@ -3,6 +3,8 @@
 Created on 16 May 2018
 
 @author: Eugen Rožić
+
+TODO
 '''
 
 import re
@@ -13,7 +15,7 @@ import numpy as np
 
 import lammps_multistate_rods
 
-def state_struct_to_id(state_struct):
+def state_struct_to_id(state_struct, model):
     '''
     Returns rod state id from its structure (list of atom types)
     '''
@@ -32,16 +34,22 @@ def wrap_periodic((elem, L)):
         elem += L
     return elem
 
-def parse_dump_file(dump_file_path):
+def parse_dump_file(dump_file_path, model, particle_offset=0, type_offset=0):
     '''
     Reads the dump file given by the path and extracts data from it.
     
-    returns : a list of [timestep, snapshot_data] pairs, where "snapshot_data" is a dictionary by
-    cluster ID's whose values are lists of (molecule ID, rod state ID) pairs
+    model : the lammps_multistate_rods.Model class instance which was used to generate the data
+    
+    returns : a list of timesteps and a corresponding list of snapshot_data, where "snapshot_data"
+    is a dictionary by cluster ID's whose values are lists of (rod/mol ID, rod state ID) pairs
+    
+    NOTE: cluster ID's are set to the lowest rod/mol ID of each cluster, not the ones that are in
+    the dump file
     '''
     with open(dump_file_path, 'r') as dump_file:
         
-        data = []
+        timesteps = []
+        raw_data = []
         #box_size = []
         #TODO get info on which column is what, don't force exact format...
         pattern = re.compile(r'(\d+) ([-\+\d\.eE]+) ([-\+\d\.eE]+) ([-\+\d\.eE]+) (\d+) (\d+) (\d+)')
@@ -54,60 +62,114 @@ def parse_dump_file(dump_file_path):
         for line in dump_file:
             i += 1
             if i == 2:
-                timestep = int(line)
+                timesteps.append(int(line))
             
             elif i == 4:
                 atoms_to_read = int(line)
                 max_row = 9 + atoms_to_read
+                min_row = 10 + particle_offset
             
 #             elif i in (6,7,8) and len(box_size) < 3:
 #                 l_bound, r_bound = [float(match) for match in re.compile(r'([-\+\d\.eE]+) ([-\+\d\.eE]+)').match(line).groups()]
 #                 box_size.append(r_bound - l_bound)
             
-            elif i >= 10 and i <= max_row:
+            elif i >= min_row and i <= max_row:
                 bead_id, x, y, z, bead_type, mol_id, cluster_id = pattern.match(line).groups()
                 mol_id = int(mol_id)
                 cluster_id = int(cluster_id)
                 if current_mol_id is None:
                     current_mol_id = mol_id
                 elif mol_id != current_mol_id:
-                    current_rod_state = state_struct_to_id(''.join(bead_type for bead_type in current_rod_structure))
+                    current_rod_state = state_struct_to_id(''.join(bead_type for bead_type in current_rod_structure), model)
                     try:
                         snapshot_data[current_cluster_id].append((current_mol_id, current_rod_state))
                     except KeyError:
                         snapshot_data[current_cluster_id] = [(current_mol_id, current_rod_state)]
                     current_mol_id = mol_id
+                    current_cluster_id = 0
                     current_rod_structure = []
-                current_rod_structure.append(bead_type)
-                if cluster_id > 0:
+                current_rod_structure.append(str(int(bead_type)-type_offset))
+                if cluster_id > current_cluster_id:
                     current_cluster_id = cluster_id
             
             if i == max_row:
-                current_rod_state = state_struct_to_id(''.join(bead_type for bead_type in current_rod_structure))
+                current_rod_state = state_struct_to_id(''.join(bead_type for bead_type in current_rod_structure), model)
                 try:
                     snapshot_data[current_cluster_id].append((current_mol_id, current_rod_state))
                 except KeyError:
                     snapshot_data[current_cluster_id] = [(current_mol_id, current_rod_state)]
-                data.append([timestep, snapshot_data])
+                
+                #switch keys to correspond to lowest mol_id in each cluster
+                temp = snapshot_data.keys()
+                for key in temp:
+                    value = snapshot_data.pop(key)
+                    snapshot_data[value[0][0]] = value
+                
+                raw_data.append(snapshot_data)
                 snapshot_data = {}
                 current_mol_id = None
                 current_cluster_id = None
                 current_rod_structure = []
                 i = 0
-    return data
+    
+    return timesteps, raw_data
 
-#TODO provide more advanced analysis (like changes in clusters between snapshots)
-def analyse_data(raw_data):
+def output_raw_data(timesteps, raw_data, output_path):
     '''
-    Analyses the output of "parse_dump_file"...
-    
-    return : a list of [timestep, cluster_sizes] pairs, where "cluster_sizes" is a dictionary by
-    cluster type (same as rod state ID if homogeneous, otherwise -1) whose values are
-    lists of (cluster size, occurrences) pairs 
+    Outputs the data returned by "parse_dump_file"
     '''
-    cluster_data = []
+    with open(output_path, 'w') as out_file:
+        for timestep, snapshot_data in zip(timesteps, raw_data):
+            out_file.write('{:^10d} | {:s}\n'.format(timestep, str(snapshot_data)))
+
+# if called as a program just parses and outputs raw rod cluster data from the given dump files
+if __name__ == "__main__":
     
-    for timestep, snapshot_data in raw_data:
+    parser = argparse.ArgumentParser(description='''
+        Application for the analysis of clusters of lammps_multistate_rods as molecules
+        from LAMMPS dump files with the output structure: id x y z type mol c_''' +
+        lammps_multistate_rods.Simulation.cluster_compute)
+    parser.add_argument('config_file', help='path to the "lammps_multistate_rods" model config file')
+    parser.add_argument('in_files', nargs='+', help='path(s) of the dump file(s) to analyse')
+    args = parser.parse_args()
+    
+    model = lammps_multistate_rods.Model(args.config_file)
+    
+    for in_file in args.in_files:
+        output_path = os.path.splitext(in_file)[0]+"_cluster_data"
+        timesteps, raw_data = parse_dump_file(in_file, model)
+        output_raw_data(timesteps, raw_data, output_path)
+
+def read_raw_data(input_path):
+    '''
+    Reads what was output with "output_raw_data"
+    
+    returns : a list of timesteps and a corresponding list of snapshot_data, where "snapshot_data"
+    is a dictionary by cluster ID's whose values are lists of (rod/mol ID, rod state ID) pairs
+    '''
+    timesteps = []
+    raw_data = []
+    
+    with open(input_path, 'r') as in_file:
+        for line in in_file:
+            timestep, snapshot_data = line.split(' | ')
+            timesteps.append(int(timestep))
+            raw_data.append(eval(snapshot_data))
+            
+    return timesteps, raw_data
+
+def cluster_sizes_by_type(raw_data):
+    '''
+    raw_data : a list of "snapshot_data", dictionaries by cluster ID whose values are lists of
+    (rod/mol ID, rod state ID) pairs
+    
+    return : a list of "cluster_sizes", dictionaries by cluster type (same as rod state ID if
+    homogeneous, otherwise -1) whose values are pairs of (cluster_sizes, occurrences) lists 
+    '''
+    ret = [None]*len(raw_data)
+    i = 0
+    
+    for snapshot_data in raw_data:
         cluster_sizes = {}
         for cluster in snapshot_data.values():
             cluster_type = cluster[0][1] # cluster_type is the same as state id of rods if they are all in the same state
@@ -120,79 +182,12 @@ def analyse_data(raw_data):
                 cluster_sizes[cluster_type] = [len(cluster)]
     
         for cluster_type, value in cluster_sizes.iteritems():
-            cluster_sizes[cluster_type] = zip(*np.unique(value, return_counts=True))
+            cluster_sizes[cluster_type] = np.unique(value, return_counts=True)
         
-        cluster_data.append([timestep, cluster_sizes])
+        ret[i] = cluster_sizes
+        i += 1
         
-    return cluster_data
+    return ret
 
-def analyse_dump_file(dump_file, config_file):
-    '''
-    return : the output of the "analyse_data" method
-    '''
-    global model
-    model = lammps_multistate_rods.Model(config_file)
+#TODO provide more advanced analysis (like changes in clusters between snapshots)
 
-    raw_data = parse_dump_file(dump_file)
-    
-    cluster_data = analyse_data(raw_data)
-    
-    return cluster_data
-
-def output_cluster_data(cluster_data, output_path):
-    
-    with open(output_path, 'w') as out_file:
-        for timestamp, cluster_sizes in cluster_data:
-            out_str = []
-            for key, value in cluster_sizes.iteritems():
-                out_str.append('({})'.format(key))
-                i = 1
-                for size, occurrence in value:
-                    while i < size:
-                        out_str.append('0')
-                        i += 1
-                    out_str.append('{:d}'.format(occurrence))
-                    i += 1
-            out_file.write('{:^10d} | {:s}\n'.format(timestamp, ' '.join(out_str)))
-
-def read_cluster_data(in_path):
-    
-    cluster_data = []
-    
-    with open(in_path, 'r') as data_file:
-        for line in data_file:
-            cluster_sizes = {}
-            timestep, raw_data = line.split(' | ')
-            chunks = re.compile(r'\(([-\d]+)\) ([ \d]+)').findall(raw_data)
-            for chunk in chunks:
-                key = int(chunk[0])
-                value = []
-                i = 1
-                for elem in chunk[1].split(' '):
-                    if elem != '':
-                        elem = int(elem)
-                        if elem > 0:
-                            value.append((i,elem))
-                        i += 1
-                cluster_sizes[key] = value
-            cluster_data.append([int(timestep), cluster_sizes])
-            
-    return cluster_data
-
-# if called as a program does the cluster analysis of a single dump file
-if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser(description='''
-        Application for the analysis of clusters of lammps_multistate_rods as molecules
-        from LAMMPS dump files with the output structure: id x y z type mol c_''' +
-        lammps_multistate_rods.Simulation.cluster_compute)
-    parser.add_argument('config_file', help='path to the "lammps_multistate_rods" model config file')
-    parser.add_argument('in_file', help='path of the dump file to analyse')
-    args = parser.parse_args()
-    
-    output_path = os.path.splitext(args.in_file)[0]+"_cluster_data"
-    
-    processed_data = analyse_dump_file(args.in_file, args.config_file)
-
-    output_cluster_data(processed_data, output_path)
-        
--- tools/cluster_statistics.py
+++ tools/cluster_statistics.py
@@ -3,135 +3,197 @@
 Created on 21 Jun 2018
 
 @author: Eugen Rožić
+
+TODO
 '''
 import os
-import numpy as np
-from math import sqrt
-from cluster_analysis import read_cluster_data
+import argparse
 
 from matplotlib import pyplot as plt
 from matplotlib.widgets import Slider
 
-import argparse
+import numpy as np
+from math import sqrt, ceil
+import cluster_analysis
 
 parser = argparse.ArgumentParser(description='''
         Application for calculating the statistics of cluster data over multiple simulations
         ''')
 parser.add_argument('in_files', nargs='+', help='paths of the *_cluster_data files')
+parser.add_argument('--bin_size', default=1, type=int, help='size of bins for doing cluster size distributions')
 args = parser.parse_args()
-
 data_dir = os.path.dirname(args.in_files[0])
-all_data = []
+bin_size = args.bin_size
+
+#get data...
+
+timesteps = None
+n_snapshots = None
+cluster_sizes_data = []
 for in_file in args.in_files:
     if data_dir != os.path.dirname(in_file):
         raise Exception('All files should be from the same directory!')
-    all_data.append(read_cluster_data(in_file))
+    _timesteps, raw_data = cluster_analysis.read_raw_data(in_file)
+    if timesteps == None:
+        timesteps = _timesteps
+        n_snapshots = len(timesteps)
+    elif len(_timesteps) != n_snapshots: #quick, non thorough check
+        raise Exception('All files should have the same timesteps!')
+    cluster_sizes_data.append(cluster_analysis.cluster_sizes_by_type(raw_data))
 
-n_snapshots = len(all_data[0])
-n_simulations = len(all_data)
+n_simulations = len(cluster_sizes_data)
 
-timesteps = [0]*n_snapshots
-avgs = [0]*n_snapshots
-avg_devs = [0]*n_snapshots
-maxs = [0]*n_snapshots
-max_devs = [0]*n_snapshots
-mcs = [0]*n_snapshots
-mc_devs = [0]*n_snapshots
-fms = [0]*n_snapshots
-fm_devs = [0]*n_snapshots
-distributions = [None]*n_snapshots
+avgs = {}
+avg_devs = {}
+maxs = {}
+max_devs = {}
+mcs = {}
+mc_devs = {}
+fms = {}
+fm_devs = {}
+distributions = {}
 
-for i in range(n_snapshots):
-    timesteps[i] = all_data[0][i][0]
+#this is a "hack", a fair and strong assumption really so I can skip exhaustive search...
+cluster_types = set()
+for sim_data in cluster_sizes_data:
+    cluster_types.update(sim_data[0].keys())
+    cluster_types.update(sim_data[n_snapshots/2].keys())
+    cluster_types.update(sim_data[-1].keys())
     
-    avg_sizes = np.zeros(n_simulations)
-    sizes_devs = np.zeros(n_simulations)
-    max_sizes = np.zeros(n_simulations, int)
-    mc_sizes = np.zeros(n_simulations, int)
-    free_mons = np.zeros(n_simulations, int)
-    for n in range(n_simulations):
-        free_mons[n] = all_data[n][i][1][0]
-        n_size_occurrences = np.array(all_data[n][i][1][1:])
-        max_sizes[n] = len(n_size_occurrences) + 1
-        if max_sizes[n] > 1: # else it should be 0, which it is by initialisation
-            n_sizes = np.arange(2, max_sizes[n]+1)
-            avg_sizes[n] = np.average(n_sizes, weights=n_size_occurrences)
-            sizes_devs[n] = sqrt(np.average((n_sizes-avg_sizes[n])**2, weights=n_size_occurrences))
-            mc_sizes[n] = np.argmax(n_size_occurrences) + 2
-    temp = np.sum(1. / sizes_devs**2)
-    avgs[i] = np.sum(avg_sizes / sizes_devs**2) / temp
-    avg_devs[i] = 1. / sqrt(temp)
-    maxs[i] = np.average(max_sizes)
-    max_devs[i] = sqrt(np.average((max_sizes-maxs[i])**2))
-    mcs[i] = np.average(mc_sizes)
-    mc_devs[i] = sqrt(np.average((mc_sizes-mcs[i])**2))
-    fms[i] = np.average(free_mons)
-    fm_devs[i] = sqrt(np.average((free_mons-fms[i])**2))
-            
-    biggest_cluster = max(max_sizes)
-    aggregate_occurrences = [None]*biggest_cluster
-    for j in range(biggest_cluster):
-        aggregate_occurrences[j] = np.zeros(n_simulations)
+# do statistics...
+
+for cluster_type in cluster_types:
+
+    avgs[cluster_type] = [0]*n_snapshots
+    avg_devs[cluster_type] = [0]*n_snapshots
+    maxs[cluster_type] = [0]*n_snapshots
+    max_devs[cluster_type] = [0]*n_snapshots
+    mcs[cluster_type] = [0]*n_snapshots
+    mc_devs[cluster_type] = [0]*n_snapshots
+    fms[cluster_type] = [0]*n_snapshots
+    fm_devs[cluster_type] = [0]*n_snapshots
+    distributions[cluster_type] = [None]*n_snapshots
+    
+    for i in range(n_snapshots):
+        avg_sizes = np.zeros(n_simulations)
+        sizes_devs = np.zeros(n_simulations)
+        max_sizes = np.zeros(n_simulations, int)
+        mc_sizes = np.zeros(n_simulations, int)
+        free_mons = np.zeros(n_simulations, int)
         for n in range(n_simulations):
             try:
-                aggregate_occurrences[j][n] = all_data[n][i][1][j]
-            except IndexError:
-                pass # if not there should be 0, which it is by initialisation
-    avg_occurrences = [np.average(elem) for elem in aggregate_occurrences]
-    dev_occurrences = [sqrt(np.average((aggregate_occurrences[j] - avg_occurrences[j])**2)) for j in range(biggest_cluster)]
-    distributions[i] = (avg_occurrences, dev_occurrences)    
+                n_sizes = np.array(cluster_sizes_data[n][i][cluster_type][0])
+            except KeyError:
+                continue
+            n_size_occurrences = np.array(cluster_sizes_data[n][i][cluster_type][1])
+            max_sizes[n] = n_sizes[-1]
+            if n_sizes[0] == 1: #if smallest size is 1 (if there are single monomers)
+                free_mons[n] = n_size_occurrences[0]
+                n_sizes = n_sizes[1:]
+                n_size_occurrences = n_size_occurrences[1:]
+            if max_sizes[n] > 1: # else it should be 0, which it is by initialisation
+                avg_sizes[n] = np.average(n_sizes, weights=n_size_occurrences)
+                sizes_devs[n] = sqrt(np.average((n_sizes-avg_sizes[n])**2, weights=n_size_occurrences))
+                mc_sizes[n] = n_sizes[np.argmax(n_size_occurrences)]
+        temp = np.sum(1. / sizes_devs**2)
+        avgs[cluster_type][i] = np.sum(avg_sizes / sizes_devs**2) / temp
+        avg_devs[cluster_type][i] = 1. / sqrt(temp)
+        maxs[cluster_type][i] = np.average(max_sizes)
+        max_devs[cluster_type][i] = sqrt(np.average((max_sizes-maxs[cluster_type][i])**2))
+        mcs[cluster_type][i] = np.average(mc_sizes)
+        mc_devs[cluster_type][i] = sqrt(np.average((mc_sizes-mcs[cluster_type][i])**2))
+        fms[cluster_type][i] = np.average(free_mons)
+        fm_devs[cluster_type][i] = sqrt(np.average((free_mons-fms[cluster_type][i])**2))
+        
+        biggest_cluster = max(max_sizes)
+        num_bins = int(ceil(float(biggest_cluster)/bin_size))
+        aggregate_occurrences = [None]*num_bins
+        for j in range(num_bins):
+            aggregate_occurrences[j] = np.zeros(n_simulations)
+        for n in range(n_simulations):
+            try:
+                for size, occurrences in zip(*cluster_sizes_data[n][i][cluster_type]):
+                    bin_no = int((size-1)/bin_size)
+                    aggregate_occurrences[bin_no][n] += occurrences
+                    #all other are 0 by initialisation
+            except KeyError:
+                pass
+        avg_occurrences = [np.average(elem) for elem in aggregate_occurrences]
+        dev_occurrences = [sqrt(np.average((aggregate_occurrences[j] - avg_occurrences[j])**2)) for j in range(num_bins)]
+        distributions[cluster_type][i] = (avg_occurrences, dev_occurrences)
 
-# drawing the static general statistics figure
-stat_fig = plt.figure('Cluster size statistics ({})'.format(data_dir)) #TODO take name of the containing folder?
-
-plt.errorbar(timesteps, avgs, yerr=avg_devs, fmt='ko-', label='average size', capsize=4, linewidth=1)
-plt.errorbar(timesteps, maxs, yerr=max_devs, fmt='ro-', label='maximum size', capsize=4, linewidth=1)
-plt.errorbar(timesteps, mcs, yerr=mc_devs, fmt='go-', label='most common size', capsize=4, linewidth=1)
-plt.errorbar(timesteps, fms, yerr=fm_devs, fmt='bo-', label='free monomers', capsize=4, linewidth=1)
-
-plt.legend(loc='upper left')
-plt.xlabel('timestep')
-plt.ylabel('number of rods', rotation='vertical')
-plt.axis(ymin=0, ymax = max(maxs)*1.2)
-plt.grid(axis='y')
-
-# drawing the dynamic clutser distribution figure
-dist_fig, ax = plt.subplots(num='Cluster size distribution ({})'.format(data_dir))
-dist_fig.subplots_adjust(bottom=0.20)
-
-plot_data = distributions[-1]
-dist_plot = ax.errorbar(range(1, len(plot_data[0])+1), plot_data[0], yerr=plot_data[1],
-                            fmt='bo-', capsize=4, linewidth=1)
-avg_lines = (ax.axvline(avgs[-1], color='black', linestyle='-', linewidth=0.5),
-             ax.axvline(avgs[-1]-avg_devs[-1], color='black', linestyle='--', linewidth=0.5),
-             ax.axvline(avgs[-1]+avg_devs[-1], color='black', linestyle='--', linewidth=0.5))
+def draw_cluster_stats(cluster_type):
     
-ax.set_xlabel('cluster size')
-ax.set_ylabel('occurrences', rotation='vertical')
+    fig = plt.figure('Cluster type {} statistics ({})'.format(cluster_type, data_dir))
 
-timestep_slider_axes = dist_fig.add_axes([0.35, 0.02, 0.32, 0.03])
-timestep_slider = Slider(timestep_slider_axes, 'timestep', 0, n_snapshots-1,
+    plt.errorbar(timesteps, avgs[cluster_type], yerr=avg_devs[cluster_type], fmt='kx-', label='average size', capsize=4, linewidth=0.7)
+    plt.errorbar(timesteps, maxs[cluster_type], yerr=max_devs[cluster_type], fmt='rx-', label='maximum size', capsize=4, linewidth=0.7)
+    plt.errorbar(timesteps, mcs[cluster_type], yerr=mc_devs[cluster_type], fmt='gx-', label='most common size', capsize=4, linewidth=0.7)
+    plt.errorbar(timesteps, fms[cluster_type], yerr=fm_devs[cluster_type], fmt='bx-', label='free monomers', capsize=4, linewidth=0.7)
+
+    plt.legend(loc='upper left')
+    plt.xlabel('timestep')
+    plt.ylabel('number of rods', rotation='vertical')
+    plt.axis(ymin=0, ymax = max(maxs[cluster_type])*1.2)
+    plt.grid(axis='y')
+    
+    return fig
+
+def fixed_size_range(size, start=0.0, step=1.0):
+    ret = [start]*size
+    i = 1
+    while i < size:
+        ret[i] = ret[i-1] + step
+        i += 1
+    return ret
+
+def draw_cluster_distributions(cluster_type):
+
+    fig, ax = plt.subplots(num='Cluster type {} size distributions ({})'.format(cluster_type, data_dir))
+    fig.subplots_adjust(bottom=0.20)
+
+    plot_data = distributions[cluster_type][-1]
+    sizes = fixed_size_range(len(plot_data[0]), np.average(range(1, bin_size+1)), bin_size)
+    ax.errorbar(sizes, plot_data[0], yerr=plot_data[1], fmt='bo-', capsize=4, linewidth=1)
+    avg = avgs[cluster_type][-1]
+    dev = avg_devs[cluster_type][-1]
+    avg_lines = (ax.axvline(avg, color='black', linestyle='-', linewidth=0.5),
+                 ax.axvline(avg-dev, color='black', linestyle='--', linewidth=0.5),
+                 ax.axvline(avg+dev, color='black', linestyle='--', linewidth=0.5))
+    
+    ax.set_xlabel('cluster size')
+    ax.set_ylabel('occurrences', rotation='vertical')
+
+    timestep_slider_axes = fig.add_axes([0.35, 0.02, 0.32, 0.03])
+    timestep_slider = Slider(timestep_slider_axes, 'timestep', 0, n_snapshots-1,
                             valinit=n_snapshots-1, valfmt="%d", valstep=1)
-timestep_slider.valtext.set_text(timestep_slider.valfmt % timesteps[-1])
+    timestep_slider.valtext.set_text(timestep_slider.valfmt % timesteps[-1])
 
-def update_dist_plot(new_timestep_index):
-    new_timestep_index = int(new_timestep_index)
-    timestep_slider.valtext.set_text(timestep_slider.valfmt % timesteps[new_timestep_index])
+    def update_dist_plot(new_timestep_index):
+        new_timestep_index = int(new_timestep_index)
+        timestep_slider.valtext.set_text(timestep_slider.valfmt % timesteps[new_timestep_index])
     
-    plot_data = distributions[new_timestep_index]
-    global dist_plot
-    dist_plot.remove()
-    dist_plot = ax.errorbar(range(1, len(plot_data[0])+1), plot_data[0], yerr=plot_data[1],
-                                fmt='bo-', capsize=4, linewidth=1)
-    ax.axis(ymax=max(plot_data[0])+max(plot_data[1]))
+        plot_data = distributions[cluster_type][new_timestep_index]
+        sizes = fixed_size_range(len(plot_data[0]), np.average(range(1, bin_size+1)), bin_size)
+        ax.containers[-1].remove()
+        ax.errorbar(sizes, plot_data[0], yerr=plot_data[1], fmt='bo-', capsize=4, linewidth=1)
+        ax.axis(ymax=max(plot_data[0])+max(plot_data[1]))
     
-    avg = avgs[new_timestep_index]
-    dev = avg_devs[new_timestep_index]
-    avg_lines[0].set_xdata([avg, avg])
-    avg_lines[1].set_xdata([avg-dev, avg-dev])
-    avg_lines[2].set_xdata([avg+dev, avg+dev])
-    dist_fig.canvas.draw_idle()
+        avg = avgs[cluster_type][new_timestep_index]
+        dev = avg_devs[cluster_type][new_timestep_index]
+        avg_lines[0].set_xdata([avg, avg])
+        avg_lines[1].set_xdata([avg-dev, avg-dev])
+        avg_lines[2].set_xdata([avg+dev, avg+dev])
+        fig.canvas.draw_idle()
     
-timestep_slider.on_changed(update_dist_plot)
+    timestep_slider.on_changed(update_dist_plot)
+    
+    return fig, timestep_slider
+
+widgets = []
+for cluster_type in cluster_types:
+    draw_cluster_stats(cluster_type)
+    fig, slider = draw_cluster_distributions(cluster_type)
+    widgets.append(slider)
 
 plt.show()
