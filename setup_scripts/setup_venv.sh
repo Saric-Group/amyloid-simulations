261512cff95ed0df0936fc2b5faad44e7652f256
Eugen Rožić <erozic@zoho.eu>
2018-08-20 23:40:32 +0100

    fixed bug in setup_venv.sh

--- setup_scripts/setup_venv.sh
+++ setup_scripts/setup_venv.sh
@@ -10,17 +10,16 @@ STARTDIR="$(pwd)"
 if [ -z "$1" ]; then
 	echo "A location for the 'venv' folder is required as an argument!"
 	exit
-else
-	VENVDIR="$1"/venv
 fi
 
-LAMMPSDIR="$(dirname "$(readlink -f "`which lammps-stable`")")"/..
+cd "$1"
+VENVDIR="`pwd`"/venv
 
 if [ ! -d $VENVDIR ]; then
 	virtualenv -p "`which python2.7`" "$VENVDIR"
 fi
 
-source "$VENVDIR/bin/activate"
+source "$VENVDIR"/bin/activate
 if [ "`which python2.7`" != "$VENVDIR/bin/python2.7" ]; then
 	echo "ERROR: Virtualenv is not there or couldn't activate. Aborting so LAMMPS doesn't install on system python. Re-run this script to finish installation after sorting out whatever is wrong with virtualenv."
 	exit
@@ -28,9 +27,10 @@ fi
 
 pip install scipy numpy pyquaternion matplotlib mpi4py
 
-cd $LAMMPSDIR/python
+LAMMPSDIR="$(dirname "$(readlink -f "`which lammps-stable`")")"/..
+
+cd "$LAMMPSDIR"/python
 python install.py
 
-cd "$STARTDIR"
-
 deactivate
+cd "$STARTDIR"
